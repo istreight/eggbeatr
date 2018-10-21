42a33d95360d5ea0dbb92b956d6b70a0396c3e93
Isaac Streight <istreight@icloud.com>
2018-10-20 20:28:53 -0700

    Default Data
    Added housing for default component data in a JSON file.
    Removed server seeders controlling default data.
    Fixed new Grid duration not being carried through by the Connector.

--- src/server/src/seeders/20180000000002-default-instructorPreferences.js
+++ src/server/src/seeders/20180000000002-default-instructorPreferences.js
@@ -1,54 +1,13 @@
 'use strict';
 
+const path = require('path');
+const defaultData = require(
+    path.resolve(process.env.INIT_CWD, 'etc/defaults/ComponentData.js')
+);
+
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('InstructorPreferences', [
-            {
-                headerId: 1,
-                instructorId: 1,
-                lessons: [
-                    'Starfish',
-                    'Duck',
-                    'Sea Turtle',
-                    'Sea Otter',
-                    'Salamander',
-                    'Sunfish',
-                    'Crocodile',
-                    'Whale'
-                ],
-                createdAt: new Date(),
-                updatedAt: new Date()
-            },
-            {
-                headerId: 1,
-                instructorId: 2,
-                lessons: [
-                    'Level 1',
-                    'Level 2',
-                    'Level 3',
-                    'Level 4',
-                    'Level 5',
-                    'Level 6',
-                    'Level 7',
-                    'Level 8',
-                    'Level 9',
-                    'Level 10'
-                ],
-                createdAt: new Date(),
-                updatedAt: new Date()
-            },
-            {
-                headerId: 1,
-                instructorId: 3,
-                lessons: [
-                    'Basics I',
-                    'Basics II',
-                    'Strokes'
-                ],
-                createdAt: new Date(),
-                updatedAt: new Date()
-            }
-        ], {});
+        return queryInterface.bulkInsert('InstructorPreferences', defaultData.InstructorPreferences, {});
     },
     down: (queryInterface, Sequelize) => {
         return queryInterface.bulkDelete('InstructorPreferences', null, {});
